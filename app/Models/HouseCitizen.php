a13e0f2e585900c4c4a386a7bb0e302d56f97ae1
andrew <andrewkalennyk@gmail.com>
2020-02-02 19:06:34 +0200

    orderBy

--- app/Models/HouseCitizen.php
+++ app/Models/HouseCitizen.php
@@ -43,7 +43,7 @@ class HouseCitizen extends Model
 
     public function street()
     {
-        return $this->belongsTo('App\Models\Street');
+        return $this->belongsTo('App\Models\Street')->orderBy('title','ASC');
     }
 
     public function citizen()
