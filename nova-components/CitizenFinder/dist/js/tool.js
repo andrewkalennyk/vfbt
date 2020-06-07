d5a0b704781dbfb1cd269a75b2937b18448839e0
andrew <andrewkalennyk@gmail.com>
2020-06-07 10:32:58 +0300

    hide new for worker

--- nova-components/CitizenFinder/dist/js/tool.js
+++ nova-components/CitizenFinder/dist/js/tool.js
@@ -4951,7 +4951,7 @@ var render = function() {
                       ]
                     ),
                     _vm._v(" "),
-                    this.userRole === "admin"
+                    this.userRole !== "worker"
                       ? _c(
                           "a",
                           {
